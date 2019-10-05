c966d3cee295da53b62346be0506685cb34584bb
Wang Guan <momocraft@gmail.com>
2019-10-05 20:49:25 +0900

    options for 'print-qr' subcommand

--- src/bin/worriedly-print-qr.ts
+++ src/bin/worriedly-print-qr.ts
@@ -1,3 +1,43 @@
-export async function worriedlyPrintQr(args: string[]) {
+import { ArgumentParser } from 'argparse';
 
+export async function worriedlyPrintQr(argv: string[]) {
+  const options = parseOptions(argv);
+  console.log(options);
 }
+
+export interface PrintQrOptions {
+  inputFile?: string;
+  outputFile?: string;
+
+  outputFormat?: 'html' | 'gif';
+}
+
+export function parseOptions(argv: string[]): PrintQrOptions {
+  const parser = new ArgumentParser({
+    version: '0.0.0',
+    addHelp: true,
+    description: 'TODO: print-qr',
+  });
+
+  parser.addArgument(['-i', '--input-file'], {
+    dest: 'inputFile',
+    metavar: 'INPUT_FILENAME',
+    help: 'Input file to read bytes from. Will read from STDIN if unspecified.',
+  });
+
+  parser.addArgument(['-o', '--output-file'], {
+    dest: 'outputFile',
+    metavar: 'FILENAME',
+    help: 'Output file to write to. Will write to STDOUT if unspecified.',
+  });
+
+  parser.addArgument(['-f', '--output-format'], {
+    dest: 'outputFile',
+    metavar: 'FORMAT',
+    help: '',
+  });
+
+  return parser.parseArgs(argv) as PrintQrOptions;
+}
+
+async function worriedlyPrintQrMain(options: PrintQrOptions) {}
--- src/bin/worriedly.ts
+++ src/bin/worriedly.ts
@@ -1,4 +1,23 @@
+import { worriedlyPrintQr } from './worriedly-print-qr';
+
 export async function cliMain() {
   const args = process.argv.slice();
-  console.log('worriedly started', args);
+
+  const [nodeBin, worriedBin, subCommand, ...rest] = args;
+
+  console.debug('worriedly started', args);
+
+  switch (subCommand) {
+    case 'print-qr':
+      return await worriedlyPrintQr(rest);
+    case undefined:
+      throw 'no subcommand specified';
+  }
+}
+
+/**
+ * to use with 'npm start:ts' script
+ */
+if (require.main === module) {
+  setTimeout(cliMain);
 }
