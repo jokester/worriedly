d4e5fc80c2f63d0e5f250b8232be4fecd59a9505
Wang Guan <momocraft@gmail.com>
2019-10-11 00:08:39 +0900

    read in bytes

--- src/bin/worriedly-print-qr.ts
+++ src/bin/worriedly-print-qr.ts
@@ -1,13 +1,20 @@
 import { ArgumentParser } from 'argparse';
+import * as fsp from '../ts-commonutil/node/fsp';
+import { readStream } from '../ts-commonutil/node';
+import { getWinstonLogger } from '../ts-commonutil/logging/winston-logger';
+
+const logger = getWinstonLogger(__filename, 'debug');
 
 export async function worriedlyPrintQr(argv: string[]) {
   const options = parseOptions(argv);
   console.log(options);
+  await worriedlyPrintQrMain(options);
 }
 
 export interface PrintQrOptions {
   inputFile?: string;
   outputFile?: string;
+  verbose?: boolean;
 
   outputFormat?: 'html' | 'gif';
 }
@@ -37,7 +44,19 @@ export function parseOptions(argv: string[]): PrintQrOptions {
     help: '',
   });
 
+  parser.addArgument(['--verbose'], {
+    defaultValue: false,
+    action: 'storeTrue',
+    dest: 'verbose',
+    help: `Enable verbose log. Most for development use.`,
+  });
+
   return parser.parseArgs(argv) as PrintQrOptions;
 }
 
-async function worriedlyPrintQrMain(options: PrintQrOptions) {}
+async function worriedlyPrintQrMain(options: PrintQrOptions) {
+  const inputBytes = options.inputFile ? await fsp.readFile(options.inputFile, { encoding: 'buffer' }) : await readStream(process.stdin);
+  if (!options.verbose) logger.silent = true;
+  logger.error('got %d bytes', inputBytes.length);
+  logger.error('got %o', inputBytes);
+}
